a4e8709025250ec04b101339b99131c933782b5c
dstanek <dawid1stanek@gmail.com>
2015-11-14 17:43:01 +0100

    Preparing for deployment

--- guardian/__main__.py
+++ guardian/__main__.py
@@ -6,34 +6,19 @@ import subprocess
 import argparse
 import logging
 
+from pkg_resources import resource_string
 from mailer import Mailer, Message
-from jinja2 import Environment, PackageLoader
+from jinja2 import Environment, FileSystemLoader
 
 import validators
 import database
 
 
 LOGGER = logging.getLogger('watch')
-CONFIG_TEMPLATE = {
-    "smtp": {
-        "host": "smtp.example.com",
-        "port": 587,
-        "user": "user@example.com",
-        "password": "secret"
-    },
-    "notify": {
-        "subject": "Failure!",
-        "email": "admin@example.com",
-        "track_last": 60
-    },
-    "servers": [
-        {
-            "name": "localhost",
-            "address": "127.0.0.1",
-            "ports": [80, 22]
-        },
-    ]
-}
+DIRECTORY = os.path.expanduser('~/.guardian/')
+DB_PATH = os.path.join(DIRECTORY, 'watch.db')
+CONFIG_PATH = os.path.join(DIRECTORY, 'config.json')
+
 
 def loglevel(value):
     try:
@@ -42,7 +27,32 @@ def loglevel(value):
         raise argparse.ArgumentTypeError("Invalid loglevel.")
 
 
-def stats(args, config, db):
+def parse_config(config_path):
+    LOGGER.info("Parsing config file: %s", config_path)
+    try:
+        with open(config_path) as f:
+            return json.loads(f.read())
+    except IOError:
+        LOGGER.error("File {0} does not exists.".format(config_path))
+        sys.exit(1)
+
+
+def install(*args):
+    LOGGER.info("Installing template files in {0} directory.".format(DIRECTORY))
+    try:
+        os.mkdir(DIRECTORY)
+    except OSError:
+        LOGGER.error('{0} already exists, please remove it manually'.format(DIRECTORY))
+        return
+    with open(os.path.join(DIRECTORY, 'config.json'), 'w') as f:
+        f.write(resource_string(__name__, 'data/config.json'))
+    with open(os.path.join(DIRECTORY, 'template.html'), 'w') as f:
+        f.write(resource_string(__name__, 'data/template.html'))
+        
+
+def stats(args):
+    config = parse_config(args.config)
+    db = database.WatchDb(db_path=DB_PATH)
     LOGGER.info("Getting server stats...")
     for server in config['servers']:
         LOGGER.info("Checking %s", server['name'])
@@ -61,14 +71,15 @@ def stats(args, config, db):
                 db.add(server['name'], validators.port.short_name, args, False)
 
 
-def notify(args, config, db):
+def notify(args):
+    config = parse_config(args.config)  
+    db = database.WatchDb(db_path=DB_PATH)
     LOGGER.info("Notify if something is not working...")
-    
     if db.get_status(config['notify']['track_last']):
         LOGGER.info("Everything seems to be ok.")
     else:
         LOGGER.warning("One of tests failed, generate report.")
-        env = Environment(loader=PackageLoader('guardian'))
+        env = Environment(loader=FileSystemLoader(DIRECTORY))
         template = env.get_template('template.html')
         body = template.render(table=db.get_latest(config['notify']['track_last']))
         LOGGER.info("Sending email...")
@@ -86,6 +97,7 @@ def notify(args, config, db):
         message.Html = body
         sender.send(message)
 
+
 def main():
     parser = argparse.ArgumentParser(__doc__)
     subparsers = parser.add_subparsers(title='subcommands')
@@ -95,7 +107,10 @@ def main():
     subparser_stats = subparsers.add_parser('stats', help="Gather statistics and save them in database.")
     subparser_stats.set_defaults(func=stats)
 
-    parser.add_argument('-c', '--config', default='~/.watcher/config.json', help='Path to config file')
+    subparser_install = subparsers.add_parser('install', help="Install template files in home directory.")
+    subparser_install.set_defaults(func=install)
+
+    parser.add_argument('-c', '--config', default=CONFIG_PATH, help='Path to config file')
     parser.add_argument('-l', '--log', default=logging.INFO,
                         type=loglevel,
                         choices=('DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL'),
@@ -108,23 +123,8 @@ def main():
         level=args.log
     )
 
-    config_path = os.path.abspath(os.path.expanduser(args.config))
-    LOGGER.info("Parsing config file: %s", config_path)
-    try:
-        with open(config_path) as f:
-            config = json.loads(f.read())
-    except IOError:
-        LOGGER.error("File {} does not exists.".format(config_path))
-        LOGGER.info("Creating new config file...")
-        os.makedirs(os.path.dirname(config_path))
-        with open(config_path, 'w') as f:
-            f.write(json.dumps(CONFIG_TEMPLATE))
-        sys.exit(1)
-
-    db = database.WatchDb(db_path=os.path.expanduser('~/.watcher/watch.db'))
-
     # Execute subcommand function
-    args.func(args, config, db)
+    args.func(args)
 
 
 if __name__ == '__main__':
--- setup.py
+++ setup.py
@@ -1,13 +1,24 @@
 from setuptools import setup
 
 setup(
-    name='guardian',
-    version='0.1.0',
-    packages=['guardian'],
+    name = 'guardian',
+    version = '0.1.0',
+    description = "Script for checking different services status.",
+    author = "Dawid Stanek",
+    author_email = 'dawid1stanek@gmail.com',
+
     include_package_data=True,
+    
+    packages=['guardian'],
     entry_points={
         'console_scripts': [
             'guardian = guardian.__main__:main'
         ]
-    } 
+    },
+
+    install_requires=[
+        'argparse',
+        'Jinja2==2.7.2',
+        'mailer==0.8.1',
+    ]  
 )
